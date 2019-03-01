ce2f0d0c613ddf2b50582e9c322d4888ee4fba6a
Maicol Esneider Bernal Pena <000404194@bga.upb>
2019-03-01 14:14:18 -0500

    creacion segundo algoritmo

---
+++ positivos.py
@@ -0,0 +1,10 @@
+a=int(input("por favor digite el numero"))
+if a==0:
+       print("el numero es 0".format(a))
+else:
+       if a%2==0:
+                print("el numero es par".format(a))
+       else:
+                print("el numero no es par".format(a))
+                
+      
